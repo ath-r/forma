7a033c3e8198394703d25d5af3a5c2e479cf7d5f
ath-r <korglp380@yandex.ru>
2025-07-11 16:35:20 +0300

    implementation separation

---
+++ source/VeronikaSynth.cpp
@@ -0,0 +1,98 @@
+#include "VeronikaSynth.h"
+
+namespace Electrophilia::Veronika
+{
+
+    VeronikaSynth::VeronikaSynth()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            voiceManager.noteOn_out (i).addMemberCallback (voices[i], &VeronikaVoice::handleNoteOn);
+            voiceManager.noteOff_out (i).addMemberCallback (voices[i], &VeronikaVoice::handleNoteOff);
+        }
+
+        parameterFluteStops1Smoother.setCutoffFrequency (100.0f);
+        parameterFluteStops2Smoother.setCutoffFrequency (100.0f);
+    }
+
+    void VeronikaSynth::setContext (Dsp::Context context)
+    {
+        for (auto& voice : voices)
+        {
+            voice.setContext (context);
+        }
+        phaseCounter.setContext (context);
+
+        timbreProcessor.setContext (context);
+        parameterFluteStops1Smoother.setContext (context);
+        parameterFluteStops2Smoother.setContext (context);
+    }
+
+    void VeronikaSynth::setParameterFlute16 (float x) { parameterFluteStops1[1] = std::lerp (minVolumeOfStop, 1.0f, x); }
+    void VeronikaSynth::setParameterFlute8 (float x) { parameterFluteStops1[1] = std::lerp (minVolumeOfStop, 1.0f, x); }
+    void VeronikaSynth::setParameterFlute4 (float x) { parameterFluteStops1[2] = std::lerp (minVolumeOfStop, 1.0f, x); }
+    void VeronikaSynth::setParameterFlute2 (float x) { parameterFluteStops1[3] = std::lerp (minVolumeOfStop, 1.0f, x); }
+
+    void VeronikaSynth::processBlock (float* buffer, int numberOfSamples)
+    {
+        if (!voiceManager.isAtLeastOneVoiceActive())
+            phaseCounter.reset();
+
+        for (int i = 0; i < numberOfSamples; i++)
+        {
+            phaseCounter.processSample();
+
+            parameterFluteStops1Smoother.process (parameterFluteStops1);
+            parameterFluteStops2Smoother.process (parameterFluteStops2);
+
+            vec4 rawSquare = 0.0f;
+            for (auto& voice : voices)
+            {
+                if (voice.isActive())
+                {
+                    vec4 sample = voice.processSample();
+                    timbreProcessor.addSignal (voice.processSample(), voice.getNote());
+
+                    rawSquare += sample;
+                }
+            }
+
+            auto sample = (timbreProcessor.processSample()) * parameterFluteStops1Smoother.last();
+            buffer[i] = sample.sum();
+
+            //This will synchronize inactive voices with global time
+            //So oscillators running at the same frequency will all be in phase
+            for (auto& voice : voices)
+            {
+                if (!voice.isActive())
+                {
+                    voice.setTime (phaseCounter.getTime());
+                }
+            }
+        }
+    }
+
+    void VeronikaSynth::handleMidiEvent (Control::Midi::Message message)
+    {
+        if (message.isNoteOnOrOff())
+        {
+            int noteNumber = message.data1;
+
+            constexpr int lowestNote = Electrophilia::Math::C1_MIDI_NOTE_NUMBER;
+            constexpr int highestNote = lowestNote + 61;
+
+            if ((noteNumber >= lowestNote) && (noteNumber <= highestNote))
+            {
+                // fire midi events here
+                if (message.isNoteOn())
+                {
+                    voiceManager.handleNoteOn (Electrophilia::Control::Midi::MessageNoteOn (message));
+                }
+                if (message.isNoteOff())
+                {
+                    voiceManager.handleNoteOff (Electrophilia::Control::Midi::MessageNoteOff (message));
+                }
+            }
+        }
+    }
+}
--- source/VeronikaSynth.h
+++ source/VeronikaSynth.h
@@ -33,121 +33,23 @@ namespace Electrophilia::Veronika
         Dsp::Filter1P<vec4> parameterFluteStops1Smoother;
         Dsp::Filter1P<vec4> parameterFluteStops2Smoother;
 
-
         const float minVolumeOfStop = Math::decibelsToAmplitude(-50);
 
 public:
+    VeronikaSynth();
 
-        VeronikaSynth()
-        {
-            for (int i = 0; i < 16; i++)
-            {
-                voiceManager.noteOn_out(i).addMemberCallback(voices[i], &VeronikaVoice::handleNoteOn);
-                voiceManager.noteOff_out(i).addMemberCallback(voices[i], &VeronikaVoice::handleNoteOff);
-            }
+    void setContext (Dsp::Context context);
 
-            parameterFluteStops1Smoother.setCutoffFrequency(100.0f);
-            parameterFluteStops2Smoother.setCutoffFrequency(100.0f);
-        }
+    void setParameterFlute16 (float x);
 
-        void setContext(Dsp::Context context)
-        {
-            for (auto& voice : voices)
-            {
-                voice.setContext(context);
-            }
-            phaseCounter.setContext(context);
+    void setParameterFlute8 (float x);
 
-            timbreProcessor.setContext(context);
-            parameterFluteStops1Smoother.setContext(context);
-            parameterFluteStops2Smoother.setContext(context);
-        }
+    void setParameterFlute4 (float x);
 
-        void setParameterFlute16(float x)
-        {
-            float a = 0.1f;
-            float b = 1.0f;
+    void setParameterFlute2 (float x);
 
-            parameterFluteStops1[0] = std::lerp(minVolumeOfStop, 1.0f, x);
+    void processBlock (float* buffer, int numberOfSamples) override;
 
-        }
-
-        void setParameterFlute8(float x)
-        {
-            parameterFluteStops1[1] = std::lerp(minVolumeOfStop, 1.0f, x);
-        }
-
-        void setParameterFlute4(float x)
-        {
-            parameterFluteStops1[2] = std::lerp(minVolumeOfStop, 1.0f, x);
-        }
-
-        void setParameterFlute2(float x)
-        {
-            parameterFluteStops1[3] = std::lerp(minVolumeOfStop, 1.0f, x);
-        }
-
-        void processBlock(float* buffer, int numberOfSamples) override
-        {
-            if (!voiceManager.isAtLeastOneVoiceActive()) phaseCounter.reset();
-
-            for (int i = 0; i < numberOfSamples; i++)
-            {
-                phaseCounter.processSample();
-
-                parameterFluteStops1Smoother.process(parameterFluteStops1);
-                parameterFluteStops2Smoother.process(parameterFluteStops2);
-
-                vec4 rawSquare = 0.0f;
-                for (auto& voice : voices)
-                {
-                    if (voice.isActive())
-                    {
-                        vec4 sample = voice.processSample();
-                        timbreProcessor.addSignal(voice.processSample(), voice.getNote());
-
-                        rawSquare += sample;
-                    }
-                }
-
-                auto sample = (timbreProcessor.processSample()) * parameterFluteStops1Smoother.last();
-                buffer[i] = sample.sum();
-
-                //This will synchronize inactive voices with global time
-                //So oscillators running at the same frequency will all be in phase
-                for (auto& voice : voices)
-                {
-                    if (!voice.isActive())
-                    {
-                        voice.setTime(phaseCounter.getTime());
-                    }
-                }
-            }
-        }
-
-        void handleMidiEvent(Control::Midi::Message message) override
-        {
-            if (message.isNoteOnOrOff())
-            {
-                int noteNumber = message.data1;
-
-                constexpr int lowestNote = Electrophilia::Math::C1_MIDI_NOTE_NUMBER;
-                constexpr int highestNote = lowestNote + 61;
-
-                if ((noteNumber >= lowestNote) && (noteNumber <= highestNote))
-                {
-                     // fire midi events here
-                     if (message.isNoteOn())
-                     {
-                        voiceManager.handleNoteOn(Electrophilia::Control::Midi::MessageNoteOn(message));
-                     }
-                     if (message.isNoteOff())
-                     {
-                        voiceManager.handleNoteOff(Electrophilia::Control::Midi::MessageNoteOff(message));
-                     }
-                }
-            }
-        }
-
+    void handleMidiEvent (Control::Midi::Message message) override;
     };
 }
