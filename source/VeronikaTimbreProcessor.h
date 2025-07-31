6073bc62f3f9d2452dd337308e647c625e67c74b
ath-r <korglp380@yandex.ru>
2025-07-31 16:49:30 +0300

    Timbre processor implementation separated

--- source/VeronikaTimbreProcessor.h
+++ source/VeronikaTimbreProcessor.h
@@ -148,60 +148,10 @@ namespace Electrophilia::Veronika
         FilterBank<3> tp6;
 
     public:
+        void setContext (Dsp::Context context);
 
-        void setContext(Dsp::Context context)
-        {
-            c = context;
+        void addSignal (vec4 in, int midiNote);
 
-            tp1.setContext(c);
-            tp1.setFrequencies(tp1freqs);
-            tp1.setDryGain(tp1SquareGain);
-
-            tp2.setContext(c);
-            tp2.setFrequencies(tp2freqs);
-            tp2.setDryGain(tp2SquareGain);
-
-            tp3.setContext(c);
-            tp3.setFrequencies(tp3freqs);
-            tp3.setDryGain(tp3SquareGain);
-
-            tp4.setContext(c);
-            tp4.setFrequencies(tp4freqs);
-            tp4.setDryGain(tp4SquareGain);
-
-            tp5.setContext(c);
-            tp5.setFrequencies(tp5freqs);
-            tp5.setDryGain(tp5SquareGain);
-
-            tp6.setContext(c);
-            tp6.setFrequencies(tp6freqs);
-            tp6.setDryGain(tp6SquareGain);
-        }
-
-        void addSignal(vec4 in, int midiNote)
-        {
-            int relativeNote = midiNote - Math::C2_MIDI_NOTE_NUMBER;
-            int input = std::clamp((relativeNote + 6) / 12, 0, 5);
-
-            inputs[input] += in;
-        }
-
-        vec4 processSample()
-        {
-            const vec4 out1 = tp1.process(inputs[0]);
-            const vec4 out2 = tp2.process(inputs[1]);
-            const vec4 out3 = tp3.process(inputs[2]);
-            const vec4 out4 = tp4.process(inputs[3]);
-            const vec4 out5 = tp5.process(inputs[4]);
-            const vec4 out6 = tp6.process(inputs[5]);
-
-            for (auto& x : inputs)
-            {
-                x = 0.0f;
-            }
-
-            return (out1 + out2 + out3 + out4 + out5 + out6) * Math::decibelsToAmplitude(-18);
-
-        }
+        vec4 processSample();
     };
 }
